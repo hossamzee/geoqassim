a8f1d44904d54f538166d36f694d45e3c85a6277
hossamzee <hossam_zee@yahoo.com>
2014-11-11 12:20:07 +0300

    Add a new method on BaseModel for having a readable timestamp.

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -3,4 +3,9 @@
 class BaseModel extends Eloquent
 {
     protected $searchable = true;
+
+    public function getReadableCreatedAtAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
