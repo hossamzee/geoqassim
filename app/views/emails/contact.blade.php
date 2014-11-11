24b20ec46e674de1eaf92b7077357e9b3f7b8317
hossamzee <hossam_zee@yahoo.com>
2014-11-11 15:35:06 +0300

    Done with fixing the message styling for now at least.

--- app/views/emails/contact.blade.php
+++ app/views/emails/contact.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="ar">
-<head>
-<meta charset="UTF-8">
-<title>Email</title>
-</head>
-<body>
-    <img src="{{ url('/assets/images/logo.png') }}" />
-    <h1>{{ $subject }}</h1>
-    <h4>{{ $name }} &lt;{{ $from }}&gt;</h4>
-    <p>
-        {{ $content }}
-    </p>
-</body>
-</html>
+@extends('layouts.emails.default')
+
+@section('subject', $subject)
+@section('name', $name)
+@section('from', $from)
+@section('content', $content)
---
+++ app/views/layouts/emails/default.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" style="direction: rtl">
+<head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <!--[if mso]>
+    <style>
+      .spacer, .border, .border td, .column-top, .column-bottom {
+        mso-line-height-rule: exactly !important;
+      }
+    </style>
+    <![endif]-->
+    <meta name="robots" content="noindex,nofollow"></meta>
+    <meta property="og:title" content="My First Campaign"></meta>
+</head>
+
+<body style="margin: 0; padding: 0;min-width: 100%;background-color: #fbfbfb">
+      <table class="wrapper" style="border-collapse: collapse;border-spacing: 0;display: table;table-layout: fixed;width: 100%;min-width: 620px;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;text-rendering: optimizeLegibility;background-color: #fbfbfb">
+        <tbody><tr>
+          <td style="padding: 0;vertical-align: top">
+            <center>
+              <table class="preheader" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto; Margin-right: auto">
+                <tbody><tr>
+                  <td style="padding: 0; vertical-align: top">
+                    <table style="border-collapse: collapse;border-spacing: 0; width: 602px">
+                      <tbody><tr>
+                        <td class="webversion" style="padding: 0;vertical-align: top;padding-top: 10px;padding-bottom: 12px;font-size: 12px;line-height: 21px;text-align: right;width: 300px;color: #999;">
+                          الرسالة قادمة من @yield('name') &lt;@yield('from')&gt;
+                        </td>
+                      </tr>
+                    </tbody></table>
+                  </td>
+                </tr>
+              </tbody></table>
+              <table class="header" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto;width: 602px">
+                <tbody><tr><td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #e9e9e9;width: 1px">&nbsp;</td></tr>
+                <tr><td class="logo" style="padding: 32px 0;vertical-align: top;font-size: 26px;font-weight: 700;letter-spacing: -0.02em;line-height: 32px;text-align: center;color: #41637e;" align="center"><center><div id="emb-email-header"><img style="border: 0;-ms-interpolation-mode: bicubic;display: block;Margin: 0 auto;max-width: 129px" src="https://i1.createsend1.com/ei/t/27/00A/820/052155/csfinal/logo.png" alt="" width="129" height="44"></div></center></td></tr>
+              </tbody></table>
+
+                  <table class="border" style="border-collapse: collapse;border-spacing: 0;font-size: 1px;line-height: 1px;background-color: #e9e9e9;Margin-left: auto;Margin-right: auto" width="602">
+                    <tbody><tr><td style="padding: 0;vertical-align: top">&#8203;</td></tr>
+                  </tbody></table>
+
+                  <table style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto">
+                    <tbody><tr>
+                      <td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #e9e9e9;width: 1px">&#8203;</td>
+                      <td style="padding: 0;vertical-align: top">
+                        <table class="one-col" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto;width: 600px;background-color: #ffffff;font-size: 14px">
+                          <tbody><tr>
+                            <td class="column" style="padding: 0;vertical-align: top;text-align: right">
+                              <div><div class="column-top" style="font-size: 32px;line-height: 32px">&nbsp;</div></div>
+                                <table class="contents" style="border-collapse: collapse;border-spacing: 0;width: 100%">
+                                  <tbody><tr>
+                                    <td class="padded" style="padding: 0;vertical-align: top;padding-left: 32px;padding-right: 32px">
+
+                    <h1 style="Margin-top: 0;color: #565656; font-size: 24px; Margin-bottom: 18px;line-height: 42px">@yield('subject')</h1>
+                    <p style="Margin-top: 0;color: #565656;font-size: 16px;line-height: 25px;Margin-bottom: 24px">@yield('content')</p>
+                                    </td>
+                                  </tr>
+                                </tbody></table>
+
+                              <div class="column-bottom" style="font-size: 8px;line-height: 8px">&nbsp;</div>
+                            </td>
+                          </tr>
+                        </tbody></table>
+                      </td>
+                      <td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #e9e9e9;width: 1px">&#8203;</td>
+                    </tr>
+                  </tbody></table>
+
+                  <table class="border" style="border-collapse: collapse;border-spacing: 0;font-size: 1px;line-height: 1px;background-color: #e9e9e9;Margin-left: auto;Margin-right: auto" width="602">
+                    <tbody><tr><td style="padding: 0;vertical-align: top">&#8203;</td></tr>
+                  </tbody></table>
+
+              <div class="spacer" style="font-size: 1px;line-height: 32px;width: 100%">&nbsp;</div>
+              <table class="footer" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto;width: 602px">
+                <tbody>
+                <tr>
+                  <td style="padding: 0;vertical-align: top">
+                    <table style="border-collapse: collapse;border-spacing: 0">
+                      <tbody><tr>
+                        <td class="address" style="padding: 0;vertical-align: top;width: 250px;padding-top: 32px;padding-bottom: 64px">
+                          <table class="contents" style="border-collapse: collapse;border-spacing: 0;width: 100%">
+                            <tbody><tr>
+                              <td class="padded" style="padding: 0;vertical-align: top;padding-left: 0;padding-right: 10px;text-align: left;font-size: 12px;line-height: 20px;color: #999;">
+
+                              </td>
+                            </tr>
+                          </tbody></table>
+                        </td>
+                        <td class="subscription" style="padding: 0;vertical-align: top;width: 350px;padding-top: 32px;padding-bottom: 64px">
+                          <table class="contents" style="border-collapse: collapse;border-spacing: 0;width: 100%">
+                            <tbody><tr>
+                              <td class="padded" style="padding: 0;vertical-align: top;padding-left: 10px;padding-right: 0;font-size: 12px;line-height: 20px;color: #999;text-align: right">
+
+                                <div>
+                                  <span class="block">
+                                    <span>
+                                      <a style="font-weight: bold;text-decoration: none;transition: all .2s;color: #999" href="{{ url() }}">
+                                        موقع قسم الجغرافيا في جامعة القصيم
+                                      </a>
+                                    </span>
+                                  </span>
+                                </div>
+                              </td>
+                            </tr>
+                          </tbody></table>
+                        </td>
+                      </tr>
+                    </tbody></table>
+                  </td>
+                </tr>
+              </tbody></table>
+            </center>
+          </td>
+        </tr>
+      </tbody></table>
+</body>
+</html>
