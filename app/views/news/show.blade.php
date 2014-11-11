c7589b9206901eb0c4705d7d439be03109c98e75
hossamzee <hossam_zee@yahoo.com>
2014-11-11 12:21:13 +0300

    Fix the readable timestamp when showing a news.

--- app/views/news/show.blade.php
+++ app/views/news/show.blade.php
@@ -9,7 +9,7 @@
         <div class="col-md-12">
             <div class="page-header">
                 <h3>{{ $news->title }}
-                    <small>({{ $news->created_at }})</small>
+                    <small>({{ $news->readable_created_at }})</small>
                     <div class="pull-left">
                         <a href="#" class="btn btn-default"><i class="fa fa-eye"></i> {{ $news->views_count }}</a>
                         <a href="{{ route('news_like', [$news->id]) }}" class="btn btn-default"><i class="fa fa-thumbs-up"></i> {{ $news->likes_count }}</a>
